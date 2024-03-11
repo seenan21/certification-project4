d593c2180fa225811a31bca31106c73a28f3bbae
Seenan <seenan21@gmail.com>
2024-03-11 14:54:55 -0700

    working on quizRoutes

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+    
+// Authentication:
+
+// /signup: Register a new user.
+// /login: Log in an existing user.
+// /logout: Log out the current user.
+// User-related routes:
+
+// /users/:userId: Retrieve user information (profile, statistics, etc.).
+// /users/:userId/quizzes: Retrieve quizzes associated with a specific user.
+// /users/:userId/quizzes/:quizId: Retrieve a specific quiz associated with a user.
+// /users/:userId/quizzes/create: Create a new quiz for a user.
+// /users/:userId/quizzes/:quizId/edit: Edit an existing quiz for a user.
+// /users/:userId/quizzes/:quizId/delete: Delete a quiz for a user.
+// Quiz-related routes:
+
+// /quizzes: Retrieve all quizzes available in the system.
+// /quizzes/:quizId: Retrieve a specific quiz.
+// /quizzes/:quizId/questions: Retrieve all questions for a specific quiz.
+// /quizzes/:quizId/questions/:questionId: Retrieve a specific question for a quiz.
+// JSON file handling routes:
+
+// /json/save: Save a quiz to a JSON file.
+// /json/load: Load a quiz from a JSON file.
